5e94e70ddc2597adbc6922fdc1dc369c4f6c0883
Yrjö Kari-Koskinen <yrjo.kari-koskinen@futurice.com>
2013-02-07 23:42:02 +0200

    deserializeTiles

--- app/models/Game.scala
+++ app/models/Game.scala
@@ -20,5 +20,16 @@ object Game {
   
   def serializeTiles(tiles: List[Tile]) =
    (for (t <- tiles) yield (t.letter + t.owner.toString)).mkString
-   
+  
+   def deserializeTiles(tiles: String): List[Tile] = {
+    def deserializeTiles0(i: Int, tiles: String): List[Tile] =
+      if (tiles.length() < 2)
+        Nil
+      else {
+        val c = tiles.charAt(0)
+        val owner = TileOwner.withName(tiles.substring(1, 2))	
+        Tile(c, i, owner) :: deserializeTiles0(i + 1, tiles.substring(2))
+    }
+    deserializeTiles0(0, tiles)
+  }
 }
--- test/unit/GameSuite.scala
+++ test/unit/GameSuite.scala
@@ -23,4 +23,27 @@ class GameSuite extends FunSuite {
 	    yield { val t = Tile(('A' + i).toChar, i, o); i += 1; t }
 	  assert(Game.serializeTiles(tiles) === "A1B2C1D2E0F3G4")
 	}
+	
+	test("deserializeTilesNeither") {
+	  val tiles = Game.deserializeTiles("A0B0C0D0E0")
+	  assert(tiles.length === 5)
+	  var i = 0
+	  tiles.foreach(t => {
+		  assert(t.letter === ('A' + i).toChar)
+		  assert(t.owner === Neither)
+		  i += 1
+	  })
+	}
+	
+	test("deserializeTiles") {
+	  val tiles = Game.deserializeTiles("A3B2C1D0E4")
+	  val owners = List(PlayerOneLocked, PlayerTwo, PlayerOne, Neither, PlayerTwoLocked)
+	  assert(tiles.length === 5)
+	  var i = 0
+	  tiles.foreach(t => {
+		  assert(t.letter === ('A' + i).toChar)
+		  assert(t.owner === owners(i))
+		  i += 1
+	  })
+	}
 }
