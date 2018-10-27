92cfd65d24852d405603e2999f15940e6c45e1ab
Yrjö Kari-Koskinen <ykk@peruna.fi>
2018-10-27 23:11:41 +0300

    select tiles

--- src/cljs/letterpress/views/game.cljs
+++ src/cljs/letterpress/views/game.cljs
@@ -4,6 +4,8 @@
             [ajax.core :refer [GET POST]]
             [cljs.tools.reader.edn :as edn]))
 
+(def selected-tiles (r/atom []))
+
 (defn- update-game-state [data]
   (reset! game (edn/read-string data)))
 
@@ -11,15 +13,32 @@
   (GET (str "/api/game/" id)
        {:handler update-game-state}))
 
+(defn- selected-tile-click! [tile]
+  (reset! selected-tiles (filterv #(not= (:id tile) (:id %)) @selected-tiles)))
+
+(defn- tile-click! [tile]
+  (swap! selected-tiles conj tile))
+
+(defn- hidden-tile? [tile]
+  (some #(= (:id tile) (:id %)) @selected-tiles))
+
+(defn- selected-tile [tile]
+  [:li
+   {:on-click #(selected-tile-click! tile)
+    :class (:owner tile)}
+   (:letter tile)])
+
 (defn- render-tile [tile size]
   (let [id (:id tile)
         left (* (mod id size) 80)
         top (+ 200 (* (quot id size) 90))]
-    [:span
-     {:style {:left (str left "px")
-              :top (str top "px")}
-      :class (:owner tile)
-      :data-id id}
+    [:li
+     {:style (conj
+              {:left (str left "px")
+               :top (str top "px")}
+              (when (hidden-tile? tile) {:display "none"}))
+      :on-click #(tile-click! tile)
+      :class (:owner tile)}
      (:letter tile)]))
 
 (defn- render-player [num name score current-turn]
@@ -114,10 +133,12 @@
     (let [size (:size @game)]
       [:div
        (render-players @game @current-player)
-       [:div
-        [:ul {:id "selected"}]]
-       [:div {:id "board"
-              :style {:width (str (* 80 size) "px")}}
+       [:ul {:id "selected"
+             :style {:width (* 80 (count @selected-tiles))}}
+        (for [tile @selected-tiles]
+          ^{:key (:id tile)} [selected-tile tile])]
+       [:ul {:id "board"
+             :style {:width (str (* 80 size) "px")}}
         (for [tile (:tiles @game)]
           ^{:key (:id tile)} [render-tile tile size])]
        [:ul {:class "playedWords"
