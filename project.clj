e9a29c7b0b9c4b0ae641e27d3d22c6dcc1da608d
Yrjö Kari-Koskinen <ykk@peruna.fi>
2018-11-02 21:47:40 +0200

    change license to MIT

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject letterpress "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A Finnish language Letterpress clone."
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT"
+            :url "https://github.com/ykarikos/letterpress.fi/blob/clj-rewrite/LICENSE"}
 
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [ring-server "0.5.0"]
